0b23a346c922f759f11bb292ec2a25ec5ccdc73f
Mohamad Mohamadi <mohamadmohamadi249@gmail.com>
2022-06-03 12:54:52 +0430

    finish main

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 import '../assets/values/AppColors.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -11,94 +13,173 @@ List<BoxShadow> box_shadow = [
   ),
 ];
 
-class MainScreen extends StatelessWidget {
-  var more_key = GlobalKey();
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      endDrawer: SafeArea(
+          child: Container(
+        width: MediaQuery.of(context).size.width / 1.5,
+        color: Colors.black,
+        height: MediaQuery.of(context).size.height,
+      )),
       backgroundColor: Color(AppColors.bg_gray),
       body: SafeArea(
-          child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(left: 20, right: 20),
-            height: 50,
-            decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(25),
-                    bottomRight: Radius.circular(25)),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 0,
-                    blurRadius: 3,
-                    offset: Offset(0, 3), // changes position of shadow
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(25),
+                      bottomRight: Radius.circular(25)),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 0,
+                      blurRadius: 3,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    child: const Icon(
+                      Icons.person_outline_rounded,
+                      size: 30,
+                    ),
+                    onTap: () => Navigator.pushNamed(context, "/profile"),
                   ),
-                ]),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Icon(
-                  Icons.person_outline_rounded,
-                  size: 30,
-                ),
-                Text(
-                  "مدرن فوتبال",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                  const Text(
+                    "مدرن فوتبال",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                  GestureDetector(
+                    onTap: () => _scaffoldKey.currentState?.openEndDrawer(),
+                    child: const Icon(
+                      Icons.menu,
+                      size: 30,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                enlargeCenterPage: true,
+                height: 180.0,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.9,
+              ),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return const LeaguesItem();
+                  },
+                );
+              }).toList(),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                MainButtonsItem(
+                  "lib/assets/images/football_icon.png",
+                  "برترین گلزنان",
                 ),
-                Icon(
-                  Icons.menu,
-                  size: 30,
+                MainButtonsItem(
+                  "lib/assets/images/ranking.png",
+                  "جدول لیگ",
+                ),
+                MainButtonsItem(
+                  "lib/assets/images/soccer_ic.png",
+                  "برنامه بازی ها",
                 )
               ],
             ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 160.0,
-              aspectRatio: 16 / 9,
-              viewportFraction: 0.9,
+            const SizedBox(
+              height: 20,
             ),
-            items: [1, 2, 3, 4, 5].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return const LeaguesItem();
-                },
-              );
-            }).toList(),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              MainButtonsItem(
-                "lib/assets/images/football_icon.png",
-                "برترین گلزنان",
+            MoreWidget("بازی های هفته جاری"),
+            SizedBox(
+              height: 10,
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 150.0,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.9,
               ),
-              MainButtonsItem(
-                "lib/assets/images/ranking.png",
-                "جدول لیگ",
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return const MatcheItem();
+                  },
+                );
+              }).toList(),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            MoreWidget("جدیدترین اخبار لیگ"),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                reverse: true,
+                children: [
+                  NewsItem(),
+                  NewsItem(),
+                  NewsItem(),
+                ],
               ),
-              MainButtonsItem(
-                "lib/assets/images/soccer_ic.png",
-                "برنامه بازی ها",
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          MoreWidget("بازی های هفته جاری")
-        ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            MoreWidget("جدیدترین ویدئو ها"),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                reverse: true,
+                children: [
+                  VideoItem(),
+                  VideoItem(),
+                  VideoItem(),
+                ],
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
@@ -234,7 +315,7 @@ class MoreWidget extends StatelessWidget {
           ),
         ),
         Container(
-          decoration: BoxDecoration(color: Colors.black),
+          decoration: BoxDecoration(color: Colors.grey),
           height: 1,
           width: MediaQuery.of(context).size.width / 2,
         ),
@@ -247,6 +328,286 @@ class MoreWidget extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class MatcheItem extends StatelessWidget {
+  const MatcheItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 10, bottom: 10),
+      padding: EdgeInsets.all(10),
+      width: 350,
+      height: 140,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+        color: Colors.white,
+        boxShadow: box_shadow,
+      ),
+      child: Row(
+        textDirection: TextDirection.rtl,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox.fromSize(
+                  size: Size(80, 80),
+                  child: Image.network(
+                      'https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/og-image.png',
+                      fit: BoxFit.cover),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "رئال مادرید",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: const [
+                  Icon(Icons.calendar_month),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text("1401/04/08")
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text("هفته 5"),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: const [
+                  Icon(Icons.access_time_rounded),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "23:30",
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              )
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox.fromSize(
+                  size: Size(60, 60),
+                  child: Image.network(
+                      'https://logos-world.net/wp-content/uploads/2020/04/Barcelona-Logo.png',
+                      fit: BoxFit.cover),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "بارسلونا",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NewsItem extends StatelessWidget {
+  const NewsItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 10, bottom: 10),
+      padding: const EdgeInsets.all(10),
+      width: 300,
+      height: 110,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+        color: Colors.white,
+        boxShadow: box_shadow,
+      ),
+      child: Stack(
+        textDirection: TextDirection.rtl,
+        children: [
+          Row(
+            textDirection: TextDirection.rtl,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox.fromSize(
+                  size: const Size.fromRadius(40),
+                  child: Image.asset('lib/assets/images/m_ch.jpg',
+                      fit: BoxFit.cover),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    "قهرمانی منچستر سیتی",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    "...لورم ایپسوم متنی ساختگی",
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                        color: Color(AppColors.primary),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(6))),
+                    child: const Text(
+                      "ورزش 3",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+          Container(
+            child: const Text(
+              "5 دقیقه پیش",
+              textDirection: TextDirection.rtl,
+              style: TextStyle(color: Colors.grey, fontSize: 11),
+            ),
+            alignment: Alignment.bottomLeft,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class VideoItem extends StatelessWidget {
+  const VideoItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 10, bottom: 10),
+      padding: const EdgeInsets.all(10),
+      width: 190,
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+        color: Colors.white,
+        boxShadow: box_shadow,
+      ),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        textDirection: TextDirection.rtl,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: SizedBox.fromSize(
+                        size: const Size(190, 110),
+                        child: Image.asset('lib/assets/images/m_ch.jpg',
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    alignment: Alignment.centerLeft,
+                    decoration:
+                        BoxDecoration(color: Colors.black.withOpacity(0.6)),
+                    child: Text(
+                      "3:30 دقیقه",
+                      style: TextStyle(color: Colors.white, fontSize: 13),
+                      textDirection: TextDirection.rtl,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "جشن قهرمانی منچسترسیتی",
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+          Row(
+            textDirection: TextDirection.rtl,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(3),
+                height: 25,
+                width: 50,
+                decoration: BoxDecoration(
+                    color: Color(AppColors.primary),
+                    borderRadius: const BorderRadius.all(Radius.circular(6))),
+                child: const Text(
+                  "ورزش 3",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Text(
+                "5 دقیقه پیش",
+                textDirection: TextDirection.rtl,
+                style: TextStyle(color: Colors.grey, fontSize: 11),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
